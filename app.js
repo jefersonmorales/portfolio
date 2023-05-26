a1b3913e4ad84d186b1c221551a376d2f5a0c72f
jefersonmorales-linux <jefersmorales@gmail.com>
2023-05-26 16:13:49 -0600

    Clean code and styles applied

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var welcomeNav = document.querySelector('#welcome-nav');
 var welcomeContainer = document.querySelector('#welcome-container');
 
-var personaldevelopmentNav = document.querySelector('#studies-nav');
-var personaldevelopmentContainer = document.querySelector('#studies-container');
+var studiesNav = document.querySelector('#studies-nav');
+var studiesContainer = document.querySelector('#studies-container');
 
+var nameToKnowmeContainer = document.querySelector('#direct-to-knowme');
 var knowmeNav = document.querySelector('#knowme-nav');
 var knowmeContainer = document.querySelector('#knowme-container');
 
@@ -11,7 +12,8 @@ var projectsNav = document.querySelector('#projects-nav');
 var projectsContainer = document.querySelector('#projects-container');
 
 welcomeNav.onclick=function() {showWelcomeContainer()}
-personaldevelopmentNav.onclick=function() {showPersonaldevelopmentContainer()}
+studiesNav.onclick=function() {showstudiesContainer()}
+nameToKnowmeContainer.onclick=function() {showKnowmeContainer()}
 knowmeNav.onclick=function() {showKnowmeContainer()}
 projectsNav.onclick=function() {showProjectsContainer()}
 
@@ -19,21 +21,21 @@ showWelcomeContainer();
 
 function showWelcomeContainer() {
     welcomeNav.classList.add("selected-item");
-    personaldevelopmentNav.classList.remove("selected-item");
+    studiesNav.classList.remove("selected-item");
     knowmeNav.classList.remove("selected-item");
     projectsNav.classList.remove("selected-item");
     welcomeContainer.classList.remove("ocultar");
-    personaldevelopmentContainer.classList.add("ocultar");
+    studiesContainer.classList.add("ocultar");
     knowmeContainer.classList.add("ocultar");
     projectsContainer.classList.add("ocultar");
 }
 
-function showPersonaldevelopmentContainer() {
-    personaldevelopmentNav.classList.add("selected-item");
+function showstudiesContainer() {
+    studiesNav.classList.add("selected-item");
     welcomeNav.classList.remove("selected-item");
     knowmeNav.classList.remove("selected-item");
     projectsNav.classList.remove("selected-item");
-    personaldevelopmentContainer.classList.remove("ocultar");
+    studiesContainer.classList.remove("ocultar");
     welcomeContainer.classList.add("ocultar");
     knowmeContainer.classList.add("ocultar");
     projectsContainer.classList.add("ocultar");
@@ -42,21 +44,21 @@ function showPersonaldevelopmentContainer() {
 function showKnowmeContainer() {
     knowmeNav.classList.add("selected-item")
     welcomeNav.classList.remove("selected-item")
-    personaldevelopmentNav.classList.remove("selected-item")
+    studiesNav.classList.remove("selected-item")
     projectsNav.classList.remove("selected-item")
     knowmeContainer.classList.remove("ocultar");
     welcomeContainer.classList.add("ocultar");
-    personaldevelopmentContainer.classList.add("ocultar");
+    studiesContainer.classList.add("ocultar");
     projectsContainer.classList.add("ocultar");
 }
 
 function showProjectsContainer() {
     projectsNav.classList.add("selected-item")
     knowmeNav.classList.remove("selected-item")
-    personaldevelopmentNav.classList.remove("selected-item")
+    studiesNav.classList.remove("selected-item")
     welcomeNav.classList.remove("selected-item")
     projectsContainer.classList.remove("ocultar");
     knowmeContainer.classList.add("ocultar");
     welcomeContainer.classList.add("ocultar");
-    personaldevelopmentContainer.classList.add("ocultar");
+    studiesContainer.classList.add("ocultar");
 }
