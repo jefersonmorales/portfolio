600bc269d674b90f2a53b388225c2f465f78b8e0
jefersonmorales-l <jefersmorales@gmail.com>
2023-05-31 17:39:55 -0600

    styles modified and images added

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var imgLogo = document.querySelector('#logo')
+var imgKnowme = document.querySelector('#image-knowme-page')
+var imgProjects = document.querySelector('#image-projects-page')
+var imgStudies = document.querySelector('#image-studies-page')
+
 var welcomeNav = document.querySelector('#welcome-nav');
 var welcomeContainer = document.querySelector('#welcome-container');
 
@@ -20,6 +25,10 @@ projectsNav.onclick=function() {showProjectsContainer()}
 showWelcomeContainer();
 
 function showWelcomeContainer() {
+    imgLogo.classList.remove("ocultar")
+    imgKnowme.classList.add("ocultar")
+    imgProjects.classList.add("ocultar")
+    imgStudies.classList.add("ocultar")
     welcomeNav.classList.add("selected-item");
     studiesNav.classList.remove("selected-item");
     knowmeNav.classList.remove("selected-item");
@@ -30,18 +39,11 @@ function showWelcomeContainer() {
     projectsContainer.classList.add("ocultar");
 }
 
-function showstudiesContainer() {
-    studiesNav.classList.add("selected-item");
-    welcomeNav.classList.remove("selected-item");
-    knowmeNav.classList.remove("selected-item");
-    projectsNav.classList.remove("selected-item");
-    studiesContainer.classList.remove("ocultar");
-    welcomeContainer.classList.add("ocultar");
-    knowmeContainer.classList.add("ocultar");
-    projectsContainer.classList.add("ocultar");
-}
-
 function showKnowmeContainer() {
+    imgLogo.classList.add("ocultar")
+    imgKnowme.classList.remove("ocultar")
+    imgProjects.classList.add("ocultar")
+    imgStudies.classList.add("ocultar")
     knowmeNav.classList.add("selected-item")
     welcomeNav.classList.remove("selected-item")
     studiesNav.classList.remove("selected-item")
@@ -52,7 +54,27 @@ function showKnowmeContainer() {
     projectsContainer.classList.add("ocultar");
 }
 
+function showstudiesContainer() {
+    imgLogo.classList.add("ocultar")
+    imgKnowme.classList.add("ocultar")
+    imgProjects.classList.add("ocultar")
+    imgStudies.classList.remove("ocultar")
+    studiesNav.classList.add("selected-item");
+    welcomeNav.classList.remove("selected-item");
+    imgProjects.classList.add("ocultar")
+    knowmeNav.classList.remove("selected-item");
+    projectsNav.classList.remove("selected-item");
+    studiesContainer.classList.remove("ocultar");
+    welcomeContainer.classList.add("ocultar");
+    knowmeContainer.classList.add("ocultar");
+    projectsContainer.classList.add("ocultar");
+}
+
 function showProjectsContainer() {
+    imgLogo.classList.add("ocultar")
+    imgKnowme.classList.add("ocultar")
+    imgProjects.classList.remove("ocultar")
+    imgStudies.classList.add("ocultar")
     projectsNav.classList.add("selected-item")
     knowmeNav.classList.remove("selected-item")
     studiesNav.classList.remove("selected-item")
